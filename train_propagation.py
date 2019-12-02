bd3aa9894f699c28295637510614be196987c768
Erik Nygren <erik.nygren@sbb.ch>
2019-12-02 15:34:10 +0100

    setting up multi train training ;)

--- train_propagation.py
+++ train_propagation.py
@@ -8,14 +8,13 @@ from graph_dataset import TrainScheduleDataset
 
 device = torch.device('cpu')
 
-schedule_data = TrainScheduleDataset("./tmp/train_schedules")
+
+schedule_data = TrainScheduleDataset('tmp/')
 schedule_loader = DataLoader(schedule_data, batch_size=6, shuffle=True)
-model = Net(features=6, n_nodes=5).to(device)
+model = Net(features=14, n_nodes=12).to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=5e-4)
 
-a = np.arange(80)
-a = torch.tensor(a)
 
 
 def train():
@@ -37,36 +36,23 @@ for epoch in range(150):
     if epoch % 100 == 0:
         print("Running epoch {} with a loss of {}".format(epoch, loss))
 
-edge_index = torch.tensor([[0, 1],
-                           [1, 0],
-                           [1, 2],
-                           [2, 1],
-                           [2, 3],
-                           [3, 2],
-                           [2, 4],
-                           [4, 2]
-                           ], dtype=torch.long)
-positions = torch.tensor([[0, 0],
-                          [0, 1],
-                          [0, 2],
-                          [0, 3],
-                          [1, 3]
-                          ], dtype=torch.long)
-current_graph = schedule_data.generate_data_point(0)
+
+current_graph = schedule_data.generate_data_point(train=0, position=0)
 
 model.eval()
-for t in range(4):
-    print("=============================================")
+for t in range(10):
+    print("====================================================")
     print("Time step number {}, agent should be at position {}".format(t, t + 1))
-    print("=============================================")
+    print("====================================================")
     if t == 0:
         nex_step = current_graph
     else:
-        nex_step = Data(x=input_data, y=input_data, edge_index=edge_index.t().contiguous(), pos=positions)
+        nex_step =schedule_data.generate_data_point(data_x=input_data, data_y=input_data)
     output = model(nex_step)
+    print(output)
     input_data = np.zeros(shape=(5, 1 + 5), dtype=float)
     input_data[:, 0] = output.detach().numpy()
     for i in range(5):
         input_data[i, i + 1] = 1
-    input_data = torch.tensor(input_data, dtype=torch.float)
-    print(input_data)
+
+    print(output.detach().numpy())
